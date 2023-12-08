7ea97d115e7c8f90289118847a8dc6ec0366a05d
mailmanjiri <mailmanjiri@gmail.com>
2023-12-07 23:56:20 -0600

    2nd commit

---
+++ src/test/java/NewConcept/GitTest2.java
@@ -0,0 +1,19 @@
+package NewConcept;
+
+public class GitTest2 {
+public static void main(String[] args) {
+	
+}
+
+public static void print() {
+	System.out.println("print1");
+}
+
+public static void print1() {
+	System.out.println("print11");
+}
+
+public static void print2() {
+	System.out.println("print2");
+}
+}
